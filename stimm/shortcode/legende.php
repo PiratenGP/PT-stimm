bef8577e5a76bb487d6d782704fd47c071765d7a
Martin Stoppler <stoppe@stoppler.de>
2015-06-21 13:34:42 +0200

    add desc support

--- stimm/shortcode/legende.php
+++ stimm/shortcode/legende.php
@@ -5,7 +5,11 @@ foreach (PT_stimm::$stimmen as $stimme) {
 	echo "<td title=\"".$stimme['name']."\" class=\"mitglied pt-stimm-stimme ".$stimme['class']."\">";
 	echo $stimme['kuerzel'];
 	echo "</td>";
-	echo "<td>".$stimme['name']."</td>";
+	if ($stimme['desc'] && ($stimme['desc'] != "")) {
+		echo "<td title=\"".htmlspecialchars($stimme['desc'])."\">".$stimme['name']."</td>";
+	} else {
+		echo "<td>".$stimme['name']."</td>";
+	}
 	echo "</tr>";
 }
 ?>
